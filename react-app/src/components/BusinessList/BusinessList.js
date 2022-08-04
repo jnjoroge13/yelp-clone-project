1771cfa4cdb38ba5d552c53286980e45b8d91bf6
Jesse <jnjoroge13@gmail.com>
2022-08-03 23:18:34 -0700

    get all business in state

--- react-app/src/components/BusinessList/BusinessList.js
+++ react-app/src/components/BusinessList/BusinessList.js
@@ -1,9 +1,14 @@
 import React, {useState, useEffect} from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { thunkGetBusinesses } from "../../store/businesses";
 
 export default function BusinessList() {
     const history = useHistory()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(thunkGetBusinesses())
+    },[])
     const newBusinessBtn = () => {
         history.push('/businesses/new')
     }
