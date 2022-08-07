54a16ca2feacf7c26d326bc75e37c3bcaaa6e03d
Jesse <jnjoroge13@gmail.com>
2022-08-07 04:29:13 -0700

    add time to review

--- react-app/src/components/Reviews/AllReviews.js
+++ react-app/src/components/Reviews/AllReviews.js
@@ -7,7 +7,33 @@ export default function AllReviews() {
 	// const dispatch = useDispatch();
 	const [reviewsArray, setReviewsArray] = useState("");
 	const reviewsSelector = useSelector((state) => state.reviews);
+    // console.log(Date.now().toUTCString())
+    function addHours(numOfHours, date = new Date()) {
+        date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
 
+        return date;
+      }
+    const convertDate = (createdAt) => {
+        const d = new Date(createdAt)
+        const addtime = addHours(4,d)
+        const saveConverted = addtime.toLocaleString();
+        // return(saveConverted)
+        // const date = createdAt.toLocaleString('en-US')
+        // // console.log(date)
+        const now = Date.now()
+        // // console.log(now.getHours())
+        // // console.log(new Date(date))
+        const hourDiff = Math.floor((now - addtime) / 1000 / 60 / 60)
+        if (hourDiff < 1) {
+            return(Math.floor((now-addtime)/1000/60)+' mins ago')
+        }
+        if (hourDiff < 24) {
+            return(Math.floor((now-addtime)/1000/60/60)+' hr ago')
+        }
+        if (hourDiff >= 24) {
+            return(Math.floor((now-addtime)/1000/60/60/24)+' day ago')
+        }
+    }
 	useEffect(() => {
 		setReviewsArray(Object.values(reviewsSelector));
 	}, [reviewsSelector]);
@@ -22,8 +48,10 @@ export default function AllReviews() {
 							id: {review.id} ;
 							userId: {review.userId} ;
 							rating: {review.rating} ;
-							review: {review.review}
-						</p>
+							review: {review.review} ;
+							created: {convertDate(review.createdAt)}
+                        </p>
+                        <button>Edit</button>
 					</div>
 				))}
 		</div>
