9f72bad7511fff2d33d1078cb110c1015b4048a0
Jesse <jnjoroge13@gmail.com>
2022-08-10 22:55:14 -0700

    add markers restaurant list

--- react-app/src/components/GoogleMaps/AllRestaurantsMap.js
+++ react-app/src/components/GoogleMaps/AllRestaurantsMap.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	GoogleMap,
+	Marker,
 	useLoadScript,
 	// Marker,
 	// InfoWindow,
 } from "@react-google-maps/api";
 import { useSelector } from "react-redux";
 
-
 const libraries = ["places"];
 
 const mapContainerStyle = {
-    width: "25vw",
-    height: "25vw",
+	width: "25vw",
+	height: "25vw",
 };
 
 const center = {
-    lat: 34.052234,
-    lng: -118.243685,
+	lat: 34.052234,
+	lng: -118.243685,
 };
 
 const options = {
-    disableDefaultUI: true,
-    zoomControl: true
-}
+	disableDefaultUI: true,
+	zoomControl: true,
+};
 export default function AllRestaurantsMap() {
 	const key = useSelector((state) => state.maps.key);
+	const restaurantSelector = useSelector((state) => state.restaurants);
+	const restaurantArray = Object.values(restaurantSelector);
+	const markers = restaurantArray.map((restaurant) =>({ lat: Number(restaurant.lat), lng: Number(restaurant.lng) })	);
 	// console.log(key)
 	// const { isLoaded, loadError } = useLoadScript({
 	// 	googleMapsApiKey: key,
@@ -39,9 +42,11 @@ export default function AllRestaurantsMap() {
 			<GoogleMap
 				mapContainerStyle={mapContainerStyle}
 				zoom={14}
-                center={center}
-                options={options}
-			></GoogleMap>
+				center={center}
+				options={options}
+			>
+				{markers.map(marker => <Marker position={marker}/>)}
+			</GoogleMap>
 		</div>
 	);
 }
